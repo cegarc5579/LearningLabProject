9cc4e437c64fc891ca04aad3b9e5209450fead2e
cegarc5579 <carlos_garcia1@baylor.edu>
2022-05-02 13:01:36 -0500

    messed with topics html

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import TopicForm
 
 from .models import Topic
 # Create your views here.
@@ -12,3 +13,24 @@ def topics(request):
     context = {'topics':topics}
 
     return render(request, 'MainApp/topics.html', context)
+
+def topic(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.all()
+    context = {'topic':topic,'entries':entries}
+
+    return render(request, 'MainApp/topic.html', context)
+
+def new_topic(request):
+    if request.method != 'POST':
+        form = TopicForm()
+    else:
+        form = TopicForm(data=request.POST)
+
+        if form.is_valid():
+            new_topic = form.save()
+
+            return redirect('MainApp:topics')
+
+    context = {'form':form}
+    return render(request, 'MainApp/new_topic.html', context)
